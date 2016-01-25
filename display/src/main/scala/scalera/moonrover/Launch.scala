c015fd5a2a18a3466814d46518e060c210f7926d
kasonchan <kasonl.chan@gmail.com>
2016-01-26 00:04:47 +0800

    Finish moonrover/display/src/main/scala/scalera/moonrover/Launch.program

--- display/src/main/scala/scalera/moonrover/Launch.scala
+++ display/src/main/scala/scalera/moonrover/Launch.scala
@@ -4,13 +4,30 @@ import scalera.moonrover.RoverProgram._
 import scalera.moonrover.interpreter.Program
 
 /**
- * Define here the program that
- * your rovers must execute
- */
+  * Define here the program that
+  * your rovers must execute
+  * Solution by Kason Chan @kasonlchan on 1/25/16.
+  */
 object Launch {
 
+  // 44
   val program = Program(
     1 -> RIGHT,
-    2 -> GOTO(1))
+    2 -> `IF FOUND PARACHUTE`(GOTO(4)),
+    3 -> GOTO(1),
+    4 -> RIGHT,
+    5 -> RIGHT,
+    6 -> RIGHT,
+    7 -> RIGHT,
+    8 -> RIGHT,
+    9 -> RIGHT,
+    10 -> RIGHT,
+    11 -> RIGHT,
+    12 -> RIGHT,
+    13 -> RIGHT,
+    14 -> RIGHT,
+    15 -> RIGHT,
+    16 -> RIGHT,
+    17 -> GOTO(4))
 
 }
